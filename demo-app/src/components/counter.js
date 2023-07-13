e09023be8c8ed65d58e03ba57baa98b5e4cce8ab
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-07-13 17:53:44 +0530

    didmount commit

--- demo-app/src/components/counter.js
+++ demo-app/src/components/counter.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 
 class valueer extends Component { 
+
+    componentDidUpdate(prevProps,prevState){
+        console.log("prevProps",prevProps);
+        console.log("prevState",prevState);
+        if (prevProps.counter.value !== this.props.counter.value){
+            //Ajax call and get new data from the server
+        }
+
+    }
+
+    componentWillUnmount(){
+        console.log("Counter-unmount");
+}
    
     render() { 
+
+        console.log("counter-rendered");
+
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatvalue()}</span>  
