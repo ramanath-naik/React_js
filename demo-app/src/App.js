93107d24294cb934eb86b4a322b9345561ed0e44
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-07-13 17:54:17 +0530

    componentdidmount

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -14,6 +14,15 @@ class App extends Component {
    
 };
 
+constructor(){
+  super();
+  console.log("App-constructor");
+}
+
+componentDidMount() {
+  console.log("App-Mounted");
+}
+
 handleIncrement = counter =>{
     const counters =[...this.state.counters];
     const index = counters.indexOf(counter);
@@ -37,6 +46,7 @@ handleDelete = counterId =>{
 };
 
   render(){
+    console.log("App-rendered");
     return (
       <>
         <NavBar totalCounters = {this.state.counters.filter(c => c.value > 0).length}/>
